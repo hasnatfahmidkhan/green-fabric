69022d4d3f5fa44966c580eed20f6e034209c595
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-11-25 18:20:45 +0600

    delete feature

--- src/app/manageProducts/page.jsx
+++ src/app/manageProducts/page.jsx
@@ -1,22 +1,46 @@
 "use client";
 import Container from "@/components/Container";
 import Heading from "@/components/Heading";
+import Modal from "@/components/Modal";
 import ProtectedRoutes from "@/components/ProtectedRoutes";
 import { useAxiosSecure } from "@/hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import { ReceiptText, Trash2 } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function ManageProducts() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [selectedTshirt, setSelectedTshirt] = useState(null);
   const axiosSecure = useAxiosSecure();
-  const { data: tShirts = [], isLoading } = useQuery({
+  const {
+    data: tShirts = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["t-shirts"],
     queryFn: async () => {
       const { data } = await axiosSecure.get("/t-shirts");
       return data;
     },
   });
+
+  const openModal = (tShirt) => {
+    setIsOpenModal(true);
+    setSelectedTshirt(tShirt);
+  };
+
+  const onConfirm = async () => {
+    const { _id } = selectedTshirt;
+    const { data } = await axiosSecure.delete(`/t-shirts/${_id}`);
+    if (data.deletedCount) {
+      toast.success("Delete Successfull");
+      refetch();
+      setIsOpenModal(false);
+    }
+  };
+
   return (
     <ProtectedRoutes>
       <Container>
@@ -67,7 +91,10 @@ export default function ManageProducts() {
                         </button>
                       </div>
                       <div className="tooltip tooltip-error" data-tip="Delete">
-                        <button className="btn btn-ghost btn-xs hover:bg-transparent border-none text-red-500">
+                        <button
+                          onClick={() => openModal(tShirt)}
+                          className="btn btn-ghost btn-xs hover:bg-transparent border-none text-red-500"
+                        >
                           <Trash2 />
                         </button>
                       </div>
@@ -77,6 +104,13 @@ export default function ManageProducts() {
               })}
             </tbody>
           </table>
+          <Modal
+            isOpen={isOpenModal}
+            onCancel={() => setIsOpenModal(false)}
+            onConfirm={onConfirm}
+            message="You want this delete this T-shirt."
+            icon={<Trash2 size={50} strokeWidth={1.3} />}
+          />
         </div>
       </Container>
     </ProtectedRoutes>
---
+++ src/components/Modal.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+export default function Modal({
+  isOpen,
+  title = "Are you sure?",
+  message = "This action cannot be undone.",
+  onConfirm,
+  onCancel,
+  icon,
+}) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal modal-open">
+      <div className="modal-box text-center">
+        <div className="flex mb-2.5 justify-center text-red-500">{icon}</div>
+        <h3 className="font-bold text-lg">{title}</h3>
+        <p className="py-4">{message}</p>
+        <div className="modal-action justify-evenly">
+          <button className="btn btn-error" onClick={onConfirm}>
+            Delete
+          </button>
+          <button className="btn" onClick={onCancel}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
